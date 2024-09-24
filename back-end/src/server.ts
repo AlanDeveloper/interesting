3443d15a248f5f10631cb7cd5bebc085de4fff1c
Alan Santos <alan.santos@yellowgo.com.br>
2024-09-23 22:17:46 -0300

    Adição de variáveis de ambiente

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,15 +1,44 @@
 import fastify from "fastify";
 import { routes } from "./routes/routes";
 import cors from "@fastify/cors";
+import fastifyEnv from "@fastify/env";
 
 export const app = fastify();
 app.register(routes);
 app.register(cors, {});
 
-app.listen({ port: 4007, host: '0.0.0.0' }, (err, address) => {
-    if (err) {
-        console.log(err);
+const schema = {
+    type: 'object',
+    required: ['APP_PORT', 'APP_HOST'],
+    properties: {
+        APP_PORT: {
+            type: 'number'
+        },
+        APP_HOST: {
+            type: 'string'
+        }
     }
+}
 
-    console.log(`Server listening at ${address}`);
+const options = {
+    dotenv: true,
+    data: process.env,
+    schema
+}
+app.register(fastifyEnv, options)
+
+app.ready(err => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    
+    app.listen({ port: Number(process.env.APP_PORT), host: process.env.APP_HOST }, (err, address) => {
+        if (err) {
+            console.log(err);
+        }
+    
+        console.log(`Server listening at ${address}`);
+    });
 });
+
